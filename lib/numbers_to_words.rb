11a64b135b53a6f639809a6206e4be7a5435d0e4
Kai deRochemont and monique st laurent <kai@>
2017-04-18 16:41:28 -0700

    add functionality to method that incorporates 10-19 and displays three digit numbers as words

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,26 +1,31 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    number_split = self.to_s.split("")
+    number_split = self.to_s().split("")
     # output_number = []
     def three_digits(arr)
-      one_digits = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
-      two_digits = { 0 => "", 1 => "ten", 2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
+      zero_to_nine = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+      ten_to_nineteen = { 0 => "ten", 1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 8 => "eighteen", 9 => "nineteen" }
+      twenty_to_ninety = { 0 => "", 1 => "", 2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
       three_digit_array = []
+
+      # ADDS ZEROS IF LESS THAN 3 DIGITS
       if arr.length().==(1)
         arr.unshift(0)
         arr.unshift(0)
       elsif arr.length().==(2)
         arr.unshift(0)
       end
-      puts arr
+
       if arr.at(0).!=(0)
-        three_digit_array.push(two_digits.fetch(arr.at(0).to_i()))
+        three_digit_array.push(zero_to_nine.fetch(arr.at(0).to_i()) + 'hundred')
       end
-      if arr.at(1).!=(0)
-        three_digit_array.push(two_digits.fetch(arr.at(1).to_i()))
+      if arr.at(1).!=("0") && arr.at(1).!=("1")
+        three_digit_array.push(twenty_to_ninety.fetch(arr.at(1).to_i()))
+      elsif arr.at(1).==("1")
+        three_digit_array.push(ten_to_nineteen.fetch(arr.at(2).to_i()))
       end
-      if arr.at(2).!=(0)
-        three_digit_array.push(one_digits.fetch(arr.at(2).to_i()))
+      if arr.at(2).!=("0") && arr.at(1).!=("1")
+        three_digit_array.push(zero_to_nine.fetch(arr.at(2).to_i()))
       end
       three_digit_array.join()
     end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('numbers_to_words')
+require('pry')
 
 
 describe('Fixnum#numbers_to_words') do
@@ -9,5 +10,9 @@ describe('Fixnum#numbers_to_words') do
 
   it("returns a string that translates a two-digit multiples of ten into written words") do
     expect(20.numbers_to_words()).to(eq("twenty"))
+  end
+
+  it("returns a string that translates a three-digit number into written words") do
+    expect(419.numbers_to_words()).to(eq("fourhundrednineteen"))
   end
 end
